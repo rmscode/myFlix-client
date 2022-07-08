4c229d675960ca15b6cc114ffb0bc7d5474bc1b2
rmscode <ricky.saull@gmail.com>
2022-07-08 13:12:58 -0400

    add "MovieView" component
    This component will display more details about a single movie once the related movie card is clicked.

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export class MovieView extends React.Component {
+  
+  render() {
+    const { movieData} = this.props;
+    
+    return (
+      <div className='movie-view'>
+        <div className='movie-poster'>
+          <img src={movieData.ImagePath} />
+        </div>
+        <div className='movie-title'>
+          <span className='label'>Title: </span>
+          <span className='value'>{movieData.Title}</span>
+        </div>
+        <div className='movie-description'>
+          <span className='label'>Description: </span>
+          <span className='value'>{movieData.Description} </span>
+        </div>
+      </div>
+    );  
+  }
+}
