9f74abc0f10eaee276a9ebb4959b10a39363e03f
rmscode <ricky.saull@gmail.com>
2022-07-09 17:59:28 -0400

    update main-view.jsx movies
    added poster art and descriptions

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,13 +9,13 @@ class MainView extends React.Component {
     this.state = {
       movies: [
         {
-          _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'
+          _id: 1, Title: 'Inception', Description: "Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life as payment for a task considered to be impossible: 'inception', the implantation of another person's idea into a target's subconscious.", ImagePath: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8IB2e4r4oVhHnANbnm7O3Tj6tF8.jpg'
         },
         {
-          _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'
+          _id: 2, Title: 'The Shawshank Redemption', Description: 'Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.', ImagePath: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg'
         },
         {
-          _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'
+          _id: 3, Title: 'Gladiator', Description: "In the year 180, the death of emperor Marcus Aurelius throws the Roman Empire into chaos. Maximus is one of the Roman army's most capable and trusted generals and a key advisor to the emperor. As Marcus' devious son Commodus ascends to the throne, Maximus is set to be executed. He escapes, but is captured by slave traders. Renamed Spaniard and forced to become a gladiator, Maximus must battle to the death with other men for the amusement of paying audiences.", ImagePath: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ty8TGRuvJLPUmAR1H1nRIsgwvim.jpg'
         }
       ],
       selectedMovie: null
