f48a859712e4b6ee0b318c85f54b777754dd3b91
rmscode <ricky.saull@gmail.com>
2022-08-20 16:06:43 -0400

    a method, onLoggedIn, passed as a prop
    onLoggedIn will be passed as a prop with the same name to LoginView. This method will update the user state of the MainView component and will be called when the user has successfully logged in. Lastly, the LoginView is rendered as long as there's no user in the state.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -35,7 +35,9 @@ class MainView extends React.Component {
   }
   
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user } = this.state;
+
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
     if (movies.length === 0) return <div className='main-view' />;
   
