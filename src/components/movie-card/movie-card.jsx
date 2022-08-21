712fa83ad23c19010fcdff3c7b309b478aad75d0
rmscode <ricky.saull@gmail.com>
2022-08-21 15:13:13 -0400

    add movie properties/PropTypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -11,7 +11,19 @@ export class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movieData: PropTypes.shape({
-    Title: PropTypes.string
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string,
+      Death: PropTypes.string
+    })
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
