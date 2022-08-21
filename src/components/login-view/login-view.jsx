a8072258730f4263fbd88adc9038ccd09503eeaf
rmscode <ricky.saull@gmail.com>
2022-08-21 17:38:59 -0400

    Import PropTypes into login-view and create props

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
@@ -24,3 +25,11 @@ export function LoginView(props) {
     </form>
   );
 }
+
+LoginView.PropTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired
+  }),
+  onLoggedIn: PropTypes.func.isRequired
+};
