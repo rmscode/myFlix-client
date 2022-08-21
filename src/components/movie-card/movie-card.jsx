fb0e025d8be7aad32710d3b931f0596e462c21a7
rmscode <ricky.saull@gmail.com>
2022-08-21 10:22:07 -0400

    set static propTypes property on MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -8,3 +8,10 @@ export class MovieCard extends React.Component {
     return <div className='movie-card' onClick={() => { onMovieClick(movieData); }}>{movieData.Title}</div>;
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
