9ade684fb70ca2164c519d17a6e21ab23da99adc
rmscode <ricky.saull@gmail.com>
2022-09-12 15:04:11 -0400

    Add views (Director, Genre, Profile) and Navbar

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import { Link } from 'react-router-dom';
+
 export class MovieCard extends React.Component {
   render() {
-    const { movieData, onMovieClick } = this.props;
+    const { movieData } = this.props;
 
     return (
-      <Card>
+      <Card className="movie-card">
         <Card.Img variant="top" src={movieData.ImagePath} />
         <Card.Body>
           <Card.Title>{movieData.Title}</Card.Title>
           <Card.Text>{movieData.Description}</Card.Text>
-          <Button onClick={() => onMovieClick(movieData)} variant="primary">More Info</Button>
         </Card.Body>
+        <Card.Footer>
+          <Link to ={`/movies/${movieData._id}`}>
+            <Button variant="primary">More info</Button> 
+          </Link>
+        </Card.Footer>
       </Card>
     );
   }
@@ -25,16 +32,6 @@ MovieCard.propTypes = {
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string
-    }),
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string,
-      Death: PropTypes.string
-    })
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
