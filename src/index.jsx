bb0e018cc7276635e499834b72deb4f5bdd6f0b9
rmscode <ricky.saull@gmail.com>
2022-09-18 16:15:28 -0400

    Fix syntax error

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
-import MainView from './components/main-view/main-view';
+import { MainView } from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
