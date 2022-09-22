559685d2188084b2a76f8ecb1910dda9e1668f59
rmscode <ricky.saull@gmail.com>
2022-09-22 16:05:42 -0400

    Update navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,55 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import Button from "react-bootstrap/Button";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
 // import './navbar.scss';
 
 export function NavBar() {
-  let user = localStorage.getItem("user");
+  let user = localStorage.getItem('user');
 
   const handleLogOut = (e) => {
     e.preventDefault();
     localStorage.clear();
-    window.open("/", "_self");
+    window.open('/', '_self');
     props.onLoggedOut(user);
   };
 
   const isAuth = () => {
-    if (typeof window == "undefined") {
+    if (typeof window == 'undefined') {
       return false;
     }
-    if (localStorage.getItem("token")) {
-      return localStorage.getItem("token");
+    if (localStorage.getItem('token')) {
+      return localStorage.getItem('token');
     } else {
       return false;
     }
   };
 
   return (
-    <Navbar collapseOnSelect expand="xxl" variant="dark">
+    <Navbar sticky='top' bg='dark' collapseOnSelect expand='xxl' variant='dark'>
       <Container>
-        <Navbar.Brand className="navbar-logo" href="/">
+        <Navbar.Brand className='navbar-logo' href='/'>
           The Jackie Chan Movie DB
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto'>
             {isAuth() && (
               <Nav.Link as={Link} to={`/users/${user}`}>
                 {user}
               </Nav.Link>
             )}
             {isAuth() && (
-              <Button className="logout" variant="link" onClick={handleLogOut}>
+              <Button className='logout' variant='link' onClick={handleLogOut}>
                 Logout
               </Button>
             )}
-            {!isAuth() && <Nav.Link href="/">Sign in</Nav.Link>}
-            {!isAuth() && <Nav.Link href="/register">Sign up</Nav.Link>}
+            {!isAuth() && <Nav.Link href='/'>Sign in</Nav.Link>}
+            {!isAuth() && <Nav.Link href='/register'>Sign up</Nav.Link>}
           </Nav>
         </Navbar.Collapse>
       </Container>
