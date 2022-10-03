bf8814ae122c15e3850b902d3b20388c32d8d1f5
rmscode <ricky.saull@gmail.com>
2022-10-03 12:57:37 -0400

    Modify MainView to use Redux to update store
    MainView no longer stores the movie list in its own state, but instead uses Redux to update the store.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import axios from 'axios';
 
+import { connect } from 'react-redux';
+
 import { 
   BrowserRouter as Router, 
-  Routes, 
   Route, 
   Redirect, 
-  Link 
 } from 'react-router-dom';
+
+import { setMovies } from '../../actions/actions';
 
 import { NavBar } from '../navbar/navbar';
 import { LoginView } from '../login-view/login-view';
-import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { DirectorView } from '../director-view/director-view';
 import { GenreView } from '../genre-view/genre-view';
@@ -23,12 +24,11 @@ import Col from 'react-bootstrap/Col';
 
 // import './main-view.scss';
 
-export class MainView extends React.Component {
+class MainView extends React.Component {
   
   constructor() {
     super();
     this.state = {
-      movies: [],
       user: null,
     };
   }
@@ -48,10 +48,8 @@ export class MainView extends React.Component {
       .get('https://jackie-chan-movie-api.herokuapp.com/movies', {
         headers: { Authorization: `Bearer ${token}` },
       })
-      .then((response) => {
-        this.setState({
-          movies: response.data,
-        });
+      .then(response => {
+        this.props.setMovies(response.data);
       })
       .catch(function (error) {
         console.log(error);
@@ -79,7 +77,8 @@ export class MainView extends React.Component {
   }
 
   render() {
-    const { movies, user } = this.state;
+    const { movies } = this.props;
+    const { user } = this.state;
     return (
       <Router>
         <NavBar user={user} />
@@ -99,11 +98,7 @@ export class MainView extends React.Component {
                   </Col>
                 );
               if (movies.length === 0) return <div className="main-view" />;
-              return movies.map((m) => (
-                <Col md={3} key={m._id}>
-                  <MovieCard movie={m} />
-                </Col>
-              ));
+              return <MoviesList movies={movies}/>;
             }}
           />
 
@@ -225,3 +220,8 @@ export class MainView extends React.Component {
   }
 }
 
+let mapStateToProps = state => {
+  return { movies: state.movies }
+}
+
+export default connect(mapStateToProps, { setMovies } )(MainView);
